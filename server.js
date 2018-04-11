b7b86903af4aacda8fd5c4028ff237a84c048398
Julia Nissen <nisse053@umn.edu>
2018-04-11 18:29:23 -0500

    updated port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var mongoose = require("mongoose");
 
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
